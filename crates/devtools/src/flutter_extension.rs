aacd482d0c4ad754eaad7d14ec34f4f7b223442a
Takeshi Tsukamoto <dev@itome.team>
2024-03-26 17:58:15 +0900

    Add render package protocols

--- crates/devtools/src/flutter_extension.rs
+++ crates/devtools/src/flutter_extension.rs
@@ -1,6 +1,8 @@
 use crate::{
     params,
-    protocols::flutter_extension::{DisplayRefreshRate, FlutterExtensionProtocol, FlutterViewList},
+    protocols::flutter_extension::{
+        DisplayRefreshRate, Dump, FlutterExtensionProtocol, FlutterViewList, Togglable,
+    },
     vm_service::VmService,
 };
 use color_eyre::Result;
@@ -16,5 +18,134 @@ impl FlutterExtensionProtocol for VmService {
             "viewId".to_owned() => view_id.into(),
         };
         self.call("_flutter.getDisplayRefreshRate", params).await
+    }
+
+    async fn invert_oversized_image(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> Result<Togglable> {
+        let params = params! {
+            "isolateId".to_owned() => isolate_id.into(),
+            "enabled".to_owned() => enabled.into(),
+        };
+        self.call("ext.flutter.invertOversizedImages", params).await
+    }
+
+    async fn debug_paint(&self, isolate_id: &str, enabled: Option<bool>) -> Result<Togglable> {
+        let params = params! {
+            "isolateId".to_owned() => isolate_id.into(),
+            "enabled".to_owned() => enabled.into(),
+        };
+        self.call("ext.flutter.debugPaint", params).await
+    }
+
+    async fn debug_paint_baseline_enabled(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> Result<Togglable> {
+        let params = params! {
+            "isolateId".to_owned() => isolate_id.into(),
+            "enabled".to_owned() => enabled.into(),
+        };
+        self.call("ext.flutter.debugPaintBaselinesEnabled", params)
+            .await
+    }
+
+    async fn repaint_rainbow(&self, isolate_id: &str, enabled: Option<bool>) -> Result<Togglable> {
+        let params = params! {
+            "isolateId".to_owned() => isolate_id.into(),
+            "enabled".to_owned() => enabled.into(),
+        };
+        self.call("ext.flutter.repaintRainbow", params).await
+    }
+
+    async fn debug_dump_layer_tree(&self, isolate_id: &str) -> Result<Dump> {
+        let params = params! {
+            "isolateId".to_owned() => isolate_id.into(),
+        };
+        self.call("ext.flutter.debugDumpLayerTree", params).await
+    }
+
+    async fn debug_disable_physical_shape_layers(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> Result<Togglable> {
+        let params = params! {
+            "isolateId".to_owned() => isolate_id.into(),
+            "enabled".to_owned() => enabled.into(),
+        };
+        self.call("ext.flutter.debugDisablePhysicalShapeLayers", params)
+            .await
+    }
+
+    async fn debug_disable_opacity_layers(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> Result<Togglable> {
+        let params = params! {
+            "isolateId".to_owned() => isolate_id.into(),
+            "enabled".to_owned() => enabled.into(),
+        };
+        self.call("ext.flutter.debugDisableOpacityLayers", params)
+            .await
+    }
+
+    async fn debug_dump_render_tree(&self, isolate_id: &str) -> Result<Dump> {
+        let params = params! {
+            "isolateId".to_owned() => isolate_id.into(),
+        };
+        self.call("ext.flutter.debugDumpRenderTree", params).await
+    }
+
+    async fn debug_dump_semantics_tree_in_traversal_order(&self, isolate_id: &str) -> Result<Dump> {
+        let params = params! {
+            "isolateId".to_owned() => isolate_id.into(),
+        };
+        self.call("ext.flutter.debugDumpSemanticsTreeInTraversalOrder", params)
+            .await
+    }
+
+    async fn debug_dump_semantics_tree_in_inverse_hit_test_order(
+        &self,
+        isolate_id: &str,
+    ) -> Result<Dump> {
+        let params = params! {
+            "isolateId".to_owned() => isolate_id.into(),
+        };
+        self.call(
+            "ext.flutter.debugDumpSemanticsTreeInInverseHitTestOrder",
+            params,
+        )
+        .await
+    }
+
+    async fn profile_render_object_paints(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> Result<Togglable> {
+        let params = params! {
+            "isolateId".to_owned() => isolate_id.into(),
+            "enabled".to_owned() => enabled.into(),
+        };
+        self.call("ext.flutter.profileRenderObjectPaints", params)
+            .await
+    }
+
+    async fn profile_render_object_layouts(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> Result<Togglable> {
+        let params = params! {
+            "isolateId".to_owned() => isolate_id.into(),
+            "enabled".to_owned() => enabled.into(),
+        };
+        self.call("ext.flutter.profileRenderObjectLayouts", params)
+            .await
     }
 }
--- crates/devtools/src/protocols/flutter_extension.rs
+++ crates/devtools/src/protocols/flutter_extension.rs
@@ -9,6 +9,72 @@ pub trait FlutterExtensionProtocol {
         &self,
         view_id: &str,
     ) -> impl Future<Output = Result<DisplayRefreshRate>> + Send;
+
+    // Service extensions for flutter rendering package
+    // https://github.com/flutter/flutter/blob/master/packages/flutter/lib/src/rendering/service_extensions.dart
+
+    fn invert_oversized_image(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> impl Future<Output = Result<Togglable>> + Send;
+
+    fn debug_paint(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> impl Future<Output = Result<Togglable>> + Send;
+
+    fn debug_paint_baseline_enabled(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> impl Future<Output = Result<Togglable>> + Send;
+
+    fn repaint_rainbow(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> impl Future<Output = Result<Togglable>> + Send;
+
+    fn debug_dump_layer_tree(&self, isolate_id: &str) -> impl Future<Output = Result<Dump>> + Send;
+
+    fn debug_disable_physical_shape_layers(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> impl Future<Output = Result<Togglable>> + Send;
+
+    fn debug_disable_opacity_layers(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> impl Future<Output = Result<Togglable>> + Send;
+
+    fn debug_dump_render_tree(&self, isolate_id: &str)
+        -> impl Future<Output = Result<Dump>> + Send;
+
+    fn debug_dump_semantics_tree_in_traversal_order(
+        &self,
+        isolate_id: &str,
+    ) -> impl Future<Output = Result<Dump>> + Send;
+
+    fn debug_dump_semantics_tree_in_inverse_hit_test_order(
+        &self,
+        isolate_id: &str,
+    ) -> impl Future<Output = Result<Dump>> + Send;
+
+    fn profile_render_object_paints(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> impl Future<Output = Result<Togglable>> + Send;
+
+    fn profile_render_object_layouts(
+        &self,
+        isolate_id: &str,
+        enabled: Option<bool>,
+    ) -> impl Future<Output = Result<Togglable>> + Send;
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
@@ -38,4 +104,18 @@ pub struct IsolateRefInFlutterExtension {
     pub number: i64,
     #[serde(rename = "fixedId")]
     pub fixed_id: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct Togglable {
+    pub r#type: String,
+    pub enabled: bool,
+    pub method: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct Dump {
+    pub r#type: String,
+    pub data: String,
+    pub method: String,
 }
