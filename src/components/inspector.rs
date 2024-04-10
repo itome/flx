a536f91a232361d37c687a9b857b2e51b7551d38
Takeshi Tsukamoto <dev@itome.team>
2024-04-10 11:03:41 +0900

    Fix inspector implementation

--- src/components/inspector.rs
+++ src/components/inspector.rs
@@ -1,12 +1,15 @@
 use std::sync::Arc;
 
 use crossterm::event::{KeyCode, KeyEvent};
+use devtools::protocols::flutter_extension::DiagnosticNode;
 use ratatui::prelude::Rect;
 use ratatui::{prelude::*, widgets::*};
 use redux_rs::Selector;
 use tokio::sync::mpsc::UnboundedSender;
+use tui_tree_widget::{Tree, TreeItem, TreeState};
 
 use crate::redux::action::Action;
+use crate::redux::selector::current_session::current_session_selector;
 use crate::redux::state::{DevTools, Focus, Home, State};
 use crate::redux::ActionOrThunk;
 use crate::tui::Frame;
@@ -16,19 +19,73 @@ use daemon::flutter::FlutterDaemon;
 use super::Component;
 
 #[derive(Default)]
-pub struct InspectorComponent {
+pub struct InspectorComponent<'a> {
     action_tx: Option<UnboundedSender<ActionOrThunk>>,
+    state: TreeState<String>,
+    items: Vec<TreeItem<'a, String>>,
 }
 
-impl InspectorComponent {
+impl<'a> InspectorComponent<'a> {
     pub fn new() -> Self {
         Self::default()
     }
+
+    fn item_builder(item: &DiagnosticNode) -> TreeItem<'a, String> {
+        if let Some(children) = item.children.as_ref() {
+            let children = children
+                .iter()
+                .map(|child| Self::item_builder(child))
+                .collect();
+            TreeItem::new(
+                item.value_id.clone().unwrap_or_default(),
+                item.description.clone().unwrap_or_default(),
+                children,
+            )
+            .unwrap()
+        } else {
+            TreeItem::new_leaf(
+                item.value_id.clone().unwrap_or_default(),
+                item.description.clone().unwrap_or_default(),
+            )
+        }
+    }
+
+    fn next(&mut self) {
+        self.state.key_down(&self.items);
+    }
+
+    fn previous(&mut self) {
+        self.state.key_up(&self.items);
+    }
+
+    fn open(&mut self) {
+        self.state.key_right();
+    }
+
+    fn close(&mut self) {
+        self.state.key_left();
+    }
+
+    fn toggle(&mut self) {
+        self.state.toggle_selected();
+    }
 }
 
-impl Component for InspectorComponent {
+impl<'a> Component for InspectorComponent<'a> {
     fn register_action_handler(&mut self, tx: UnboundedSender<ActionOrThunk>) -> Result<()> {
         self.action_tx = Some(tx);
+        Ok(())
+    }
+
+    fn handle_key_events(&mut self, key: &KeyEvent, state: &State) -> Result<()> {
+        match key.code {
+            KeyCode::Char('j') | KeyCode::Down => self.next(),
+            KeyCode::Char('k') | KeyCode::Up => self.previous(),
+            KeyCode::Char('l') | KeyCode::Right => self.open(),
+            KeyCode::Char('h') | KeyCode::Left => self.close(),
+            KeyCode::Enter => self.toggle(),
+            _ => {}
+        }
         Ok(())
     }
 
@@ -46,6 +103,27 @@ impl Component for InspectorComponent {
             .border_type(BorderType::Rounded)
             .borders(Borders::ALL);
 
-        f.render_widget(block, area);
+        let Some(session) = current_session_selector(state) else {
+            f.render_widget(block.clone(), area);
+            return;
+        };
+
+        self.items = session
+            .widget_summary_tree
+            .iter()
+            .map(|node| Self::item_builder(node))
+            .collect::<Vec<_>>();
+
+        let tree = Tree::new(self.items.clone())
+            .unwrap()
+            .block(block)
+            .experimental_scrollbar(Some(Scrollbar::new(ScrollbarOrientation::VerticalRight)))
+            .highlight_style(if state.focus == Focus::DevTools(DevTools::Inspector) {
+                Style::default().bg(Color::DarkGray)
+            } else {
+                Style::default()
+            });
+
+        f.render_stateful_widget(tree, area, &mut self.state);
     }
 }
