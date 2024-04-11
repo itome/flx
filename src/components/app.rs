22432e99ce798e8a2628468ad47f2c908d133951
Takeshi Tsukamoto <dev@itome.team>
2024-04-11 15:29:00 +0900

    Run hotrestart from devtool

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,27 +1,67 @@
 use std::sync::Arc;
 
+use crossterm::event::{KeyCode, KeyEvent};
 use ratatui::prelude::Rect;
 use ratatui::{prelude::*, widgets::*};
 use redux_rs::Selector;
+use tokio::sync::mpsc::UnboundedSender;
 
 use crate::redux::selector::current_session::current_session_selector;
 use crate::redux::state::{DevTools, Focus, State};
+use crate::redux::thunk::ThunkAction;
+use crate::redux::ActionOrThunk;
 use crate::tui::Frame;
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{eyre, Result};
 use daemon::flutter::FlutterDaemon;
 
 use super::Component;
 
 #[derive(Default)]
-pub struct AppComponent {}
+pub struct AppComponent {
+    action_tx: Option<UnboundedSender<ActionOrThunk>>,
+}
 
 impl AppComponent {
     pub fn new() -> Self {
         Self::default()
     }
+
+    fn hot_reload(&self) -> Result<()> {
+        self.action_tx
+            .as_ref()
+            .ok_or_else(|| eyre!("action_tx is None"))?
+            .send(ThunkAction::HotReload.into())?;
+        Ok(())
+    }
+
+    fn hot_restart(&self) -> Result<()> {
+        self.action_tx
+            .as_ref()
+            .ok_or_else(|| eyre!("action_tx is None"))?
+            .send(ThunkAction::HotRestart.into())?;
+        Ok(())
+    }
 }
 
 impl Component for AppComponent {
+    fn register_action_handler(&mut self, tx: UnboundedSender<ActionOrThunk>) -> Result<()> {
+        self.action_tx = Some(tx);
+        Ok(())
+    }
+
+    fn handle_key_events(&mut self, key: &KeyEvent, state: &State) -> Result<()> {
+        if !matches!(state.focus, Focus::DevTools(_)) || state.popup.is_some() {
+            return Ok(());
+        }
+
+        match key.code {
+            KeyCode::Char('r') => self.hot_reload()?,
+            KeyCode::Char('R') => self.hot_restart()?,
+            _ => {}
+        }
+        Ok(())
+    }
+
     fn draw(&mut self, f: &mut Frame<'_>, area: Rect, state: &State) {
         let Some(session) = current_session_selector(state) else {
             return;
